9e0a71f1c2cb40a46040405efcaa1be0d01dda15
SAND <112147283+SANDtcs@users.noreply.github.com>
2024-10-08 20:08:08 +0530

    Create hits.py

---
+++ hits.py
@@ -0,0 +1,28 @@
+import numpy as np
+import numpy.linalg as la
+
+def hits(adjMat):
+    mat1 = adjMat.dot(adjMat.T)
+    mat2 = adjMat.T.dot(adjMat)
+    eVals1, eVecs1 = la.eig(mat1)
+    eVals2, eVecs2 = la.eig(mat2)
+    order1 = np.absolute(eVals1).argsort()[::-1]
+    order2 = np.absolute(eVals2).argsort()[::-1]
+    eVals1 = eVals1[order1]
+    eVals2 = eVals2[order2]
+    eVecs1 = eVecs1[:, order1]
+    eVecs2 = eVecs2[:, order2]
+    auths = eVecs1[:, 0]
+    hubs = eVecs2[:, 0]
+
+    return np.real(hubs / np.sum(hubs)), np.real(auths / np.sum(auths))
+
+adjMat = np.array([
+    [0, 1, 1, 1],
+    [0, 0, 1, 1],
+    [1, 0, 0, 1],
+    [0, 0, 0, 1]
+])
+hubs, auths = hits(adjMat)
+print("Hub Scores:", hubs)
+print("Authority Scores:", auths)
